32383983d1e88ab0ea422e452ae647ab16b91ca2
Kamil Baldyga <baldygakamil@gmail.com>
2025-09-08 14:18:07 +0200

    add layout nav & footer

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,14 +1,18 @@
 import './bootstrap';
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
+import Layout from '../components/Layout';
 
 createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.jsx');
-        return pages[`./Pages/${name}.jsx`];
+        return pages[`./Pages/${name}.jsx`]();
     },
     setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />);
+        createRoot(el).render(
+            <Layout>
+                <App {...props} />
+            </Layout>
+        );
     },
 });
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-return Inertia::render('Pages/page.jsx', [
-'message' => 'Bienvenue dans Inertia 🚀',
-]);
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
